4c1ae03f5eb49599e6446485ca1d8bbf8550bc54
Francesco Battaglia <fpbattaglia@gmail.com>
2014-11-15 22:52:36 +0100

    some functional test refactoring

--- superlists/lists/views.py
+++ superlists/lists/views.py
@@ -2,4 +2,5 @@ from django.shortcuts import render
 # Create your views here.
 
 def home_page(request):
-    return render(request, 'home.html')
+    return render(request, 'home.html',
+        {'new_item_text': "1: " + request.POST.get('item_text', '')})
