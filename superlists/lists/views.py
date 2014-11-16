a7c7b7df959760a27d2a03cfade5cd662ad30eed
Francesco Battaglia <fpbattaglia@gmail.com>
2014-11-16 17:38:11 +0100

    ORM now works

--- superlists/lists/views.py
+++ superlists/lists/views.py
@@ -1,10 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from lists.models import Item
 # Create your views here.
 
 def home_page(request):
-    ret_item = request.POST.get('item_text', '')
-    if ret_item:
-        ret_item = "1: " + ret_item
+    if request.method == 'POST':
+        Item.objects.create(text=request.POST['item_text'])
+        return redirect('/')
 
+    items = Item.objects.all()
+    print "no POST here "
+    print "item count", len(items)
     return render(request, 'home.html',
-        {'new_item_text': ret_item})
+        {'items': items})
