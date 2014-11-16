6e06b4811c227ebd0fb69ef0b2c42685f052d94c
Francesco Battaglia <fpbattaglia@gmail.com>
2014-11-16 22:27:26 +0100

    adding a new view

--- superlists/lists/views.py
+++ superlists/lists/views.py
@@ -2,23 +2,18 @@ from django.shortcuts import render, redirect
 from lists.models import Item
 # Create your views here.
 
-def home_page(request):
-    if request.method == 'POST':
-        Item.objects.create(text=request.POST['item_text'])
-        return redirect('/lists/the-only-list-in-the-world')
 
+def home_page(request):
     items = Item.objects.all()
     return render(request, 'home.html',
-        {'items': items})
-
-# def view_list(request):
-#     print 'baubau'
-#     items = Item.objects.all()
-#     print 'bau'
-#     return render(request, 'home.html',
-#     {'items': items})
+                  {'items': items})
 
 
 def view_list(request):
     items = Item.objects.all()
     return render(request, 'list.html', {'items': items})
+
+
+def new_list(request):
+    Item.objects.create(text=request.POST['item_text'])
+    return redirect('/lists/the-only-list-in-the-world')
