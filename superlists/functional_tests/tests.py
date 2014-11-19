5b8d836d4f2a5a9ef4f2c634d95a9009801f3190
Francesco Battaglia <fpbattaglia@gmail.com>
2014-11-19 16:37:08 +0100

    making first aesthetic test

--- superlists/functional_tests/tests.py
+++ superlists/functional_tests/tests.py
@@ -85,5 +85,22 @@ class NewVisitorTest(LiveServerTestCase):
         self.assertNotIn('make a fly', page_text)
 
         # back to sleep
+    def test_layout_and_styling(self):
+        # go to home page
+        self.browser.get(self.live_server_url)
+        self.browser.set_window_size(1024, 768)
 
+        # input box is centered
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        self.assertAlmostEqual(
+            inputbox.location['x'] + inputbox.size['width'] /2,
+            512,
+            delta = 5
+        )
 
+        inputbox.send_keys('testing\n')
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        self.assertAlmostEqual(inputbox.location['x'] + inputbox.size['width'] / 2,
+                               512,
+                               delta=5
+        )
