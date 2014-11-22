db9acac9a23355d4aaf645c5b6604cbce61dd104
Francesco Battaglia <fpbattaglia@gmail.com>
2014-11-22 16:53:29 +0100

    written test for empty items and started refactor lists/tests

--- functional_tests/test_list_item_validation.py
+++ functional_tests/test_list_item_validation.py
@@ -7,18 +7,28 @@ class ItemValidationTest(FunctionalTest):
 
     def test_cannot_add_empty_list_items(self):
         # user tries to submit an empty list item, hits Enter on empty imput box
+        self.browser.get(self.server_url)
+        self.browser.find_element_by_id('id_new_item').send_keys('\n')
 
         # home page refreshes and there is an error message saying that list item cannot be
         #  blank
-
+        error = self.browser.find_element_by_css_selector('.has-error')
+        self.assertEqual(error.text, "You can't have an empty list item")
 
         # trying again for some text, and that works
+        self.browser.find_element_by_id('id_new_item').send_keys('Buy milk\n')
+        self.check_for_row_in_list_table('1: Buy milk')
 
         # and a second blank item
+        self.browser.find_element_by_id('id_new_item').send_keys('\n')
 
         #another warning
+        error = self.browser.find_element_by_css_selector('.has-error')
+        self.assertEqual(error.text, "You can't have an empty list item")
 
         # and some text is filled in
-
+        self.browser.find_element_by_id('id_new_item').send_keys('Make tea\n')
+        self.check_for_row_in_list_table('1: Buy milk')
+        self.check_for_row_in_list_table('2: Make tea')
 
         self.fail('Write me!')
