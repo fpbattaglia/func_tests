8e8f71961c48c27ec1bb599f74ea4191ef2637af
Francesco Battaglia <fpbattaglia@gmail.com>
2014-11-24 09:40:57 +0100

    fixed js os that it only runs with page fully loaded

--- lists/static/list.js
+++ lists/static/list.js
@@ -2,6 +2,8 @@
  * Created by fpbatta on 24/11/2014.
  */
 
-$('input').on('keypress', function() {
-    $('.has-error').hide();
+jQuery(document).ready( function($) {
+    $('input').on('keypress', function () {
+        $('.has-error').hide();
+    });
 });
