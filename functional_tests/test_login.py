5e54b532077ffc1859849f207e07258917d56051
Francesco Battaglia <fpbattaglia@gmail.com>
2014-12-01 16:31:14 +0100

    now functional test is included

---
+++ functional_tests/test_login.py
@@ -0,0 +1,43 @@
+__author__ = 'fpbatta'
+from .base import FunctionalTest
+from selenium.webdriver.support.ui import WebDriverWait
+import time
+
+
+class LoginTest(FunctionalTest):
+    def switch_to_new_window(self, text_in_title):
+        retries = 60
+        while retries > 0:
+            for handle in self.browser.window_handles:
+                self.browser.switch_to.window(handle)
+                if text_in_title in self.browser.title:
+                    return
+            retries -= 1
+            time.sleep(0.5)
+        self.fail('could not find window')
+
+    def wait_for_element_with_id(self, element_id):
+        WebDriverWait(self.browser, timeout=30).until(
+            lambda b: b.find_element_by_id(element_id)
+        )
+
+    def test_login_with_persona(self):
+        #finds a sign in link and clicks it
+        self.browser.get(self.server_url)
+        self.browser.find_element_by_id('id_login').click()
+
+        # A personal window appears, switch to it
+        self.switch_to_new_window('Mozilla Persona')
+
+        #Edith logs in with her email address
+        self.browser.find_element_by_id('authentication_email'
+        ).send_keys('fpb@mockmyid.com')
+        self.browser.find_element_by_tag_name('button').click()
+
+        # close the Persona window
+        self.switch_to_new_window('To-Do')
+
+        #she's logged in
+        self.wait_for_element_with_id('id_logout')
+        navbar = self.browser.find_element_by_css_selector('.navbar')
+        self.assertIn('fpb@mockmyid.com', navbar.text)
