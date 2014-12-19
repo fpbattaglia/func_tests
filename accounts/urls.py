13d7312d55ba8280d63ff4b9e125243ad36e3767
Francesco Battaglia <fpbattaglia@gmail.com>
2014-12-19 21:00:14 +0100

    first test of login passes

---
+++ accounts/urls.py
@@ -0,0 +1,6 @@
+__author__ = 'fpbatta'
+
+from django.conf.urls import patterns, url
+
+urlpatterns = patterns('',
+                       url(r'^login$', 'accounts.views.persona_login', name='persona_login'))
