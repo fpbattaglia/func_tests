21e79d1078727f051335e049f28e7ebdea40c33c
Francesco Battaglia <fpbattaglia@gmail.com>
2014-12-23 22:39:22 +0100

    placeholder test_my_lists

--- functional_tests/test_login.py
+++ functional_tests/test_login.py
@@ -1,8 +1,7 @@
 __author__ = 'fpbatta'
 from .base import FunctionalTest
-from selenium.webdriver.support.ui import WebDriverWait
 import time
-
+TEST_EMAIL='fpb@mockmyid.com'
 
 class LoginTest(FunctionalTest):
     def switch_to_new_window(self, text_in_title):
@@ -16,24 +15,6 @@ class LoginTest(FunctionalTest):
             time.sleep(0.5)
         self.fail('could not find window')
 
-    def wait_for_element_with_id(self, element_id):
-        WebDriverWait(self.browser, timeout=30).until(
-            lambda b: b.find_element_by_id(element_id),
-            'Could not find element with id {}. Page text was {}'.format(
-                element_id, self.browser.find_element_by_tag_name('body').text
-            )
-        )
-
-    def wait_to_be_logged_in(self):
-        self.wait_for_element_with_id('id_logout')
-        navbar = self.browser.find_element_by_css_selector('.navbar')
-        self.assertIn('fpb@mockmyid.com', navbar.text)
-
-    def wait_to_be_logged_out(self):
-        self.wait_for_element_with_id('id_login')
-        navbar = self.browser.find_element_by_css_selector('.navbar')
-        self.assertNotIn('fpb@mockmyid.com', navbar.text)
-
     def test_login_with_persona(self):
         #finds a sign in link and clicks it
         self.browser.get(self.server_url)
@@ -44,22 +25,22 @@ class LoginTest(FunctionalTest):
 
         #Edith logs in with her email address
         self.browser.find_element_by_id('authentication_email'
-        ).send_keys('fpb@mockmyid.com')
+        ).send_keys(TEST_EMAIL)
         self.browser.find_element_by_tag_name('button').click()
 
         # close the Persona window
         self.switch_to_new_window('To-Do')
 
         #she's logged in
-        self.wait_to_be_logged_in()
+        self.wait_to_be_logged_in(email=TEST_EMAIL)
 
         # refresh the page and try again
         self.browser.refresh()
-        self.wait_to_be_logged_in()
+        self.wait_to_be_logged_in(email=TEST_EMAIL)
 
         self.browser.find_element_by_id('id_logout').click()
-        self.wait_to_be_logged_out()
+        self.wait_to_be_logged_out(email=TEST_EMAIL)
 
         # refresh and try again
         self.browser.refresh()
-        self.wait_to_be_logged_out()
+        self.wait_to_be_logged_out(email=TEST_EMAIL)
